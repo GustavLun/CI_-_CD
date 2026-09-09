33ddcf63100c179b4191aedad15416219e53a59a
GustavLundh <gustav.lundh@student.nbi-handelsakademin.se>
2026-09-09 15:17:36 +0200

    Assigment 2 test again

--- src/Register.py
+++ src/Register.py
@@ -2,7 +2,6 @@ class Register:
     def __init__(self):
         self.registers = []
 
-
     def register_new_register(self, name, cd):
         self.registers.append(name)
         cd.add_name_in_registry(name)
