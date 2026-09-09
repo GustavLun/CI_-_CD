2f663d817afe5c3641fb266be1fc3a24939b540b
GustavLundh <gustav.lundh@student.nbi-handelsakademin.se>
2026-09-09 15:25:48 +0200

    Assigment 2 test again 5

--- tests/unit/test_register.py
+++ tests/unit/test_register.py
@@ -1,13 +1,11 @@
-
 import pytest
 from src.Register import Register
 
-
 @pytest.mark.Unit
 def test_add_name_in_registry(mocker):
-  register = Register()
-  name = "Jesus"
-  cd = mocker.Mock()
-  register.register_new_register(name,cd)
+    register = Register()
+    name = "Jesus"
+    cd = mocker.Mock()
+    register.register_new_register(name, cd)
 
-  assert "Jesus" in register.registers
+    assert "Jesus" in register.registers
